53704f771dc33effcc2b0e7b6ac6504cdf5b19eb
Dario Gallegos <dariogal@ucm.es>
2019-07-05 17:47:06 +0200

    It is the adapter used by the RecyclerView of list to see to form the items of list, such us a text and image, and to can make horizontal in groups.

---
+++ app/src/main/java/com/example/psymood/Activities/MyItemListAdapter.java
@@ -0,0 +1,54 @@
+package com.example.psymood.Activities;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.psymood.Models.ItemData;
+import com.example.psymood.R;
+
+import java.util.List;
+
+public class MyItemListAdapter extends RecyclerView.Adapter<MyItemListAdapter.MyViewHolder> {
+    private Context context;
+    private List<ItemData> itemDataList;
+
+    public MyItemListAdapter(Context context, List<ItemData> itemDataList) {
+        this.context = context;
+        this.itemDataList = itemDataList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_list, viewGroup , false);
+        MyItemListAdapter.MyViewHolder viewHolder = new MyItemListAdapter.MyViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        myViewHolder.text_view_item.setText(itemDataList.get(i).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (itemDataList != null ? itemDataList.size() : 0) ;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView text_view_item;
+        ImageView img_item;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            text_view_item  = itemView.findViewById(R.id.itemText);
+            img_item = itemView.findViewById(R.id.itemImage);
+        }
+    }
+}
