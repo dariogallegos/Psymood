f418cf2a8db2c74d5dceb21bb45aed7506c7c326
Dario Gallegos <dariogal@ucm.es>
2019-07-23 03:17:47 +0200

    A setOnClickListener is added to each task, so when pressed, it goes to the indicated fragment.

--- app/src/main/java/com/example/psymood/Activities/MyTaskAdapter.java
+++ app/src/main/java/com/example/psymood/Activities/MyTaskAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,17 +21,20 @@ import java.util.List;
 public class MyTaskAdapter extends RecyclerView.Adapter<MyTaskAdapter.ViewHolder> {
 
     private List<ItemTask> itemTaskList;
+    private OnTaskClickListener mListener;
     private Context context;
 
 
-    public MyTaskAdapter(Context context,List<ItemTask> itemTaskList) {
+    public MyTaskAdapter(Context context,List<ItemTask> itemTaskList,OnTaskClickListener mListener) {
         this.context =  context;
         this.itemTaskList = itemTaskList;
+        this.mListener = mListener;
     }
 
 
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_task, parent, false);
         MyTaskAdapter.ViewHolder viewHolder = new MyTaskAdapter.ViewHolder(v);
         //ViewHolder viewHolder = new ViewHolder(v);
@@ -38,10 +42,10 @@ public class MyTaskAdapter extends RecyclerView.Adapter<MyTaskAdapter.ViewHolder
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        holder.bind(itemTaskList.get(position),mListener);
 
-        //holder.cardTask.setBackground(new ColorDrawable(itemTaskList.get(position).getColorTask()));
         holder.iconOfTask.setBackgroundResource(itemTaskList.get(position).getIconTask());
         holder.cardTask.setCardBackgroundColor(ContextCompat.getColor(context,itemTaskList.get(position).getColorTask()));
         holder.titleTask.setText(itemTaskList.get(position).getTitleTask());
@@ -72,5 +76,21 @@ public class MyTaskAdapter extends RecyclerView.Adapter<MyTaskAdapter.ViewHolder
             cardTask = itemView.findViewById(R.id.cardViewTask);
 
         }
+
+        public void bind(final ItemTask itemTask, final OnTaskClickListener mlistener) {
+
+            //itemView es el item del ViewHolder
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mlistener.onTaskClick(itemTask);
+                }
+            });
+
+        }
+    }
+
+    public interface OnTaskClickListener{
+        void onTaskClick(ItemTask itemTask);
     }
 }
